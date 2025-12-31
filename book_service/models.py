b6e193bb9c8e3126bb33598142c58a22a08348ed
Serhii Chumak <s4umak@gmail.com>
2025-12-31 16:05:34 +0200

    Added models in book_servise and rename library_service

---
+++ book_service/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from decimal import Decimal
+
+
+class Book(models.Model):
+    class CoverChoices(models.TextChoices):
+        HARD = "Hardcover"
+        SOFT = "Softcover"
+
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    cover = models.CharField(
+        max_length=15,
+        choices=CoverChoices.choices,
+        default=CoverChoices.HARD
+    )
+    inventory = models.PositiveIntegerField(
+        default=0,
+        help_text="Number of copies available in the book_service."
+    )
+    daily_fee = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        default=Decimal("0.00")
+    )
+
+    def __str__(self):
+        return f"{self.title}-{self.author} ({self.inventory} pcs)"
+
+
+    class Meta:
+        ordering = ["title"]
