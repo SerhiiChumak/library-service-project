dee0a1e5b3754de0f3f20a2256b81d5619c2e18b
Serhii Chumak <s4umak@gmail.com>
2026-01-02 10:12:49 +0200

    Modified all files with black

--- book_service/models.py
+++ book_service/models.py
@@ -10,23 +10,17 @@ class Book(models.Model):
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
     cover = models.CharField(
-        max_length=15,
-        choices=CoverChoices.choices,
-        default=CoverChoices.HARD
+        max_length=15, choices=CoverChoices.choices, default=CoverChoices.HARD
     )
     inventory = models.PositiveIntegerField(
-        default=0,
-        help_text="Number of copies available in the book_service."
+        default=0, help_text="Number of copies available in the book_service."
     )
     daily_fee = models.DecimalField(
-        max_digits=10,
-        decimal_places=2,
-        default=Decimal("0.00")
+        max_digits=10, decimal_places=2, default=Decimal("0.00")
     )
 
     def __str__(self):
         return f"{self.title}-{self.author} ({self.inventory} pcs)"
-
 
     class Meta:
         ordering = ["title"]
